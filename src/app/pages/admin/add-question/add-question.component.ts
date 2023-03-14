56dd739f29a8658fd12002830fa7f3fa62118f13
nahuelvera00 <nahuelvera3770@gmail.com>
2023-03-14 17:00:40 -0300

    add form question

--- src/app/pages/admin/add-question/add-question.component.ts
+++ src/app/pages/admin/add-question/add-question.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-add-question',
   templateUrl: './add-question.component.html',
-  styleUrls: ['./add-question.component.css']
+  styleUrls: ['./add-question.component.css'],
 })
-export class AddQuestionComponent {
+export class AddQuestionComponent implements OnInit {
+  quizId: any;
+  quizTitle: any;
 
+  question: any = {
+    quiz: {},
+    content: '',
+    option1: '',
+    option2: '',
+    option3: '',
+    option4: '',
+    answer: '',
+  };
+
+  constructor(private _route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.quizId = this._route.snapshot.paramMap.get('qid');
+    this.quizTitle = this._route.snapshot.paramMap.get('title');
+
+    console.log(this.quizId);
+    this.question.quiz['qid'] = this.quizId;
+  }
 }
